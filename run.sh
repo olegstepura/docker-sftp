5bddf20d7cca654dba9b01a8565c98c53697174e
Oleg Stepura <github@oleg.stepura.com>
2016-02-14 02:56:56 +0200

    Fix wrong id used for month in a date command

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ DIR=$(dirname $(realpath $0))
 
 VAR=IMAGE docker_select_image
 VAR=CONTAINER_NAME docker_enter_container_name
-VAR=LOCALPATH DESC="sftp user root" PLACEHOLDER="/docker/www/$(date +"%Y-%M")/" docker_enter_dir
+VAR=LOCALPATH DESC="sftp user root" PLACEHOLDER="/docker/www/$(date +"%Y-%m")/" docker_enter_dir
 VAR=SSH_PORT DESC="SSH" PLACEHOLDER="22" docker_enter_port
 
 ARGUMENTS="--restart=always --publish $SSH_PORT:22 --volume $LOCALPATH:/srv/www/" docker_run
